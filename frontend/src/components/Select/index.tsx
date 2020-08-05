4dc15cf60cb17f7f242058c821f74effe8081373
Davi Félix <davi.felix.lima@gmail.com>
2020-08-05 17:17:37 -0300

    Created separated select component

---
+++ frontend/src/components/Select/index.tsx
@@ -0,0 +1,34 @@
+import React, { SelectHTMLAttributes } from 'react'
+
+import './styles.css'
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+    name: string,
+    label: string,
+    message: string,
+    options: Array<{
+        value: string,
+        label: string,
+    }>,
+}
+
+const Select: React.FC<SelectProps> = ({ name, label, options, message, ...props }) => {
+    return (
+        <div className="select-block">
+            <select name={name} id={name} placeholder=" " {...props} >
+                <option value="" disabled selected hidden>
+                    {message}
+                </option>
+                {options.map(option => (
+                    <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
+            </select>
+            <label htmlFor={name}>{label}</label>
+        </div>
+    )
+}
+
+export default Select
+
